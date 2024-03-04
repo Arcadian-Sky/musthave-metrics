30f8a57b248ee26be0514a0d48c96a08ba43e20e
ArcadianSky <lllarcadialll@gmail.com>
2024-03-05 00:23:57 +0400

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Arcadian-Sky/musthave-metrics/internal/server"
+	// "runtime"
+	// "strconv"
+	// "sync"
+)
+
+// Пример запроса к серверу:
+// POST /update/counter/someMetric/527 HTTP/1.1
+// Host: localhost:8080
+// Content-Length: 0
+// Content-Type: text/plain
+
+// Пример ответа от сервера:
+// HTTP/1.1 200 OK
+// Date: Tue, 21 Feb 2023 02:51:35 GMT
+// Content-Length: 11
+// Content-Type: text/plain; charset=utf-8
+
+// func main() {
+// 	fmt.Println("Hello from othercmd application!")
+// 	package2.FunctionFromPackage2()
+// }
 
 func main() {
-	fmt.Printf("hello, world\n")
+	server.InitServer()
 }
