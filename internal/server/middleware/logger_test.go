c6cda51f7367eca9e6d7ab0ac992d256b5806913
ArcadianSky <Arcadian.Sky@yahoo.com>
2024-03-26 01:12:24 +0400

    go vet ^?

--- internal/server/middleware/logger_test.go
+++ internal/server/middleware/logger_test.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -23,16 +24,22 @@ func Test_loggingResponseWriter_Write(t *testing.T) {
 
 	data := []byte("test data")
 	gotN, err := lrw.Write(data)
-
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
-	}
-	if gotN != len(data) {
-		t.Errorf("Unexpected number of bytes written. Expected %d, got %d", len(data), gotN)
 	}
 
 	res := recorder.Result()
 	defer res.Body.Close()
+
+	_, err = io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+
+	if gotN != len(data) {
+		t.Errorf("Unexpected number of bytes written. Expected %d, got %d", len(data), gotN)
+	}
+
 }
 
 func Test_loggingResponseWriter_WriteHeader(t *testing.T) {
@@ -45,15 +52,19 @@ func Test_loggingResponseWriter_WriteHeader(t *testing.T) {
 			size:   0,
 		},
 	}
-
 	lrw.WriteHeader(http.StatusOK)
+	res := recorder.Result()
+	defer res.Body.Close()
+
+	_, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
 
 	if recorder.Result().StatusCode != http.StatusOK {
 		t.Errorf("Unexpected status code. Expected %d, got %d", http.StatusOK, recorder.Result().StatusCode)
 	}
 
-	res := recorder.Result()
-	defer res.Body.Close()
 }
 
 func TestGetLogger(t *testing.T) {
