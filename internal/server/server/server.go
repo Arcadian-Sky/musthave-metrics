261d54a4a1c25aba25d1e608b12688734b9d7e6f
ArcadianSky <Arcadian.Sky@yahoo.com>
2024-03-30 12:35:51 +0400

    try fix  iteration7_test.go

--- internal/server/server/server.go
+++ internal/server/server/server.go
@@ -52,7 +52,7 @@ func InitRouter(handler handler.Handler) chi.Router {
 	})
 	r.Route("/value", func(r chi.Router) {
 		r.Post("/", handler.GetMetricsJSONHandlerFunc)
-		r.Get("/", handler.GetMetricsHandlerFunc)
+		// r.Get("/", handler.GetMetricsHandlerFunc)
 		r.Route("/{type}", func(r chi.Router) {
 			r.Get("/", handler.GetMetricsHandlerFunc)
 			r.Get("/{name}", handler.GetMetricsHandlerFunc)
