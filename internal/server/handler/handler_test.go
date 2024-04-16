9b7e78418badc1e1d43dc875b142e43ae33cbd26
ArcadianSky <Arcadian.Sky@yahoo.com>
2024-04-16 16:45:04 +0400

    more tests

--- internal/server/handler/handler_test.go
+++ internal/server/handler/handler_test.go
@@ -1,12 +1,15 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/Arcadian-Sky/musthave-metrics/internal/server/models"
 	"github.com/Arcadian-Sky/musthave-metrics/internal/server/storage/inmemory"
 	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
@@ -72,18 +75,22 @@ func InitRouter() chi.Router {
 		r.Header.Set("Content-Type", "text/plain")
 	})
 	// GET http://localhost:8080/value/counter/testSetGet163
-
 	r.Get("/", handler.MetricsHandlerFunc)
+	r.Get("/ping", handler.PingDB)
+	r.Post("/updates/", handler.UpdateJSONMetricsHandlerFunc)
+
 	r.Route("/update", func(r chi.Router) {
-		r.Post("/", handler.UpdateMetricsHandlerFunc)
+		r.Post("/", handler.UpdateJSONMetricHandlerFunc)
 		r.Route("/{type}", func(r chi.Router) {
 			r.Post("/", handler.UpdateMetricsHandlerFunc)
 			r.Post("/{name}", handler.UpdateMetricsHandlerFunc)
+			r.Post("/{name}/", handler.UpdateMetricsHandlerFunc)
 			r.Post("/{name}/{value}", handler.UpdateMetricsHandlerFunc)
 			r.Post("/{name}/{value}/", handler.UpdateMetricsHandlerFunc)
+			r.Get("/{name}/{value}/", handler.UpdateMetricsHandlerFunc)
+
 		})
 	})
-
 	r.Route("/value", func(r chi.Router) {
 		r.Post("/", handler.GetMetricsJSONHandlerFunc)
 		r.Get("/", handler.GetMetricHandlerFunc)
@@ -228,24 +235,24 @@ func TestHandler_UpdateMetricsHandlers(t *testing.T) {
 			requestPath:   "/update/gauge/someName/",
 			expectedType:  "count",
 			expectedName:  "someName",
-			expectedCode:  404,
-			expectedValue: "404",
+			expectedCode:  400,
+			expectedValue: "invalid",
 		},
 		{
 			name:          "not valid request 2",
 			requestPath:   "/update/counter/someName/",
 			expectedType:  "count",
 			expectedName:  "someName",
-			expectedCode:  404,
-			expectedValue: "404",
+			expectedCode:  400,
+			expectedValue: "invalid",
 		},
 		{
 			name:          "not valid request 3",
 			requestPath:   "/update/error/someName/",
 			expectedType:  "",
 			expectedName:  "",
-			expectedCode:  404,
-			expectedValue: "404",
+			expectedCode:  400,
+			expectedValue: "invalid metric type",
 		},
 		{
 			name:          "not valid request count no name",
@@ -276,8 +283,8 @@ func TestHandler_UpdateMetricsHandlers(t *testing.T) {
 			requestPath:   "/update/",
 			expectedType:  "",
 			expectedName:  "",
-			expectedCode:  404,
-			expectedValue: "metric type not provided",
+			expectedCode:  400,
+			expectedValue: "Request body is empty",
 		},
 	}
 
@@ -346,6 +353,208 @@ func TestHandler_MetricsHandlerFunc(t *testing.T) {
 			respBody, _ := io.ReadAll(response.Body)
 			fmt.Println(string(respBody))
 			assert.Contains(t, string(respBody), tt.expectedValue)
+			defer response.Body.Close()
+		})
+	}
+}
+
+func TestHandler_UpdateMetricsJSONHandlerFunc(t *testing.T) {
+	value := 100.0
+	var delta int64 = 100
+	// var value0 float64 = 0
+	tests := []struct {
+		requestPath     string
+		name            string
+		requestBody     string
+		expectedCode    int
+		expectedMetrics models.Metrics
+	}{
+		{
+			requestPath:  "/value",
+			name:         "valid request gauge",
+			requestBody:  `{"id": "metric1", "type": "gauge", "value": 100}`,
+			expectedCode: http.StatusOK,
+			expectedMetrics: models.Metrics{
+				ID:    "metric1",
+				MType: "gauge",
+				Value: &value,
+			},
+		},
+		{
+			requestPath:  "/value",
+			name:         "valid request counter",
+			requestBody:  `{"id": "metric2", "type": "counter", "delta": 100}`,
+			expectedCode: http.StatusOK,
+			expectedMetrics: models.Metrics{
+				ID:    "metric1",
+				MType: "gauge",
+				Delta: &delta,
+			},
+		},
+		{
+			requestPath:     "/value",
+			name:            "empty request",
+			requestBody:     `{}`, // Empty request body
+			expectedCode:    http.StatusBadRequest,
+			expectedMetrics: models.Metrics{},
+		},
+		{
+			requestPath:  "/value",
+			name:         "only ID request",
+			requestBody:  `{"id": "metric1"}`,
+			expectedCode: http.StatusBadRequest,
+			expectedMetrics: models.Metrics{
+				ID: "metric1",
+				// Value: &value0,
+			},
+		},
+		{
+			requestPath:  "/value",
+			name:         "ID and type request",
+			requestBody:  `{"id": "metric1", "type": "gauge"}`,
+			expectedCode: http.StatusOK,
+			expectedMetrics: models.Metrics{
+				ID:    "metric1",
+				MType: "gauge",
+				// Value: &value0,
+			},
+		},
+	}
+
+	testServer := httptest.NewServer(InitRouter())
+	defer testServer.Close()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			requestBody := bytes.NewBufferString(tt.requestBody)
+			request, err := http.NewRequest(http.MethodPost, testServer.URL+"/update/", requestBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+			request.Header.Set("Content-Type", "application/json")
+
+			response, err := http.DefaultClient.Do(request)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t, tt.expectedCode, response.StatusCode)
+
+			var result models.Metrics
+			if tt.expectedCode == http.StatusOK {
+				fmt.Printf("response.Body: %v\n", response.Body)
+				err := json.NewDecoder(response.Body).Decode(&result)
+				if err != nil {
+					t.Fatal(err)
+				}
+				fmt.Printf("result: %v\n", result)
+
+				var buf bytes.Buffer
+				encoder := json.NewEncoder(&buf)
+				assert.Equal(t, encoder.Encode(result), encoder.Encode(tt.expectedMetrics))
+			}
+
+			defer response.Body.Close()
+		})
+	}
+}
+
+func TestHandler_GetMetricsJSONHandlerFunc(t *testing.T) {
+	value := 100.0
+	var delta int64 = 100
+	// var value0 float64 = 0
+	tests := []struct {
+		requestPath     string
+		name            string
+		requestBody     string
+		expectedCode    int
+		expectedMetrics models.Metrics
+	}{
+		{
+			requestPath:  "/value",
+			name:         "valid request",
+			requestBody:  `{"id": "metric1", "type": "gauge", "value": 100}`,
+			expectedCode: http.StatusOK,
+			expectedMetrics: models.Metrics{
+				ID:    "metric1",
+				MType: "gauge",
+				Value: &value,
+			},
+		},
+		{
+			requestPath:  "/value",
+			name:         "valid request",
+			requestBody:  `{"id": "metric2", "type": "counter", "delta": 100}`,
+			expectedCode: http.StatusOK,
+			expectedMetrics: models.Metrics{
+				ID:    "metric1",
+				MType: "gauge",
+				Delta: &delta,
+			},
+		},
+		{
+			requestPath:     "/value",
+			name:            "empty request",
+			requestBody:     `{}`, // Empty request body
+			expectedCode:    http.StatusOK,
+			expectedMetrics: models.Metrics{},
+		},
+		{
+			requestPath:  "/value",
+			name:         "only ID request",
+			requestBody:  `{"id": "metric1"}`,
+			expectedCode: http.StatusOK,
+			expectedMetrics: models.Metrics{
+				ID: "metric1",
+				// Value: &value0,
+			},
+		},
+		{
+			requestPath:  "/value",
+			name:         "ID and type request",
+			requestBody:  `{"id": "metric1", "type": "gauge"}`,
+			expectedCode: http.StatusOK,
+			expectedMetrics: models.Metrics{
+				ID:    "metric1",
+				MType: "gauge",
+				// Value: &value0,
+			},
+		},
+	}
+
+	testServer := httptest.NewServer(InitRouter())
+	defer testServer.Close()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			requestBody := bytes.NewBufferString(tt.requestBody)
+			request, err := http.NewRequest(http.MethodPost, testServer.URL+"/value", requestBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+			request.Header.Set("Content-Type", "application/json")
+
+			response, err := http.DefaultClient.Do(request)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t, tt.expectedCode, response.StatusCode)
+
+			var result models.Metrics
+			if tt.expectedCode == http.StatusOK {
+				// fmt.Printf("response.Body: %v\n", response.Body)
+				err := json.NewDecoder(response.Body).Decode(&result)
+				if err != nil {
+					t.Fatal(err)
+				}
+				fmt.Printf("result: %v\n", result)
+
+				var buf bytes.Buffer
+				encoder := json.NewEncoder(&buf)
+				assert.Equal(t, encoder.Encode(result), encoder.Encode(tt.expectedMetrics))
+			}
+
 			defer response.Body.Close()
 		})
 	}
