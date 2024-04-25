c17ca4b48c0661cada09f402ee868684332e91f6
ArcadianSky <Arcadian.Sky@yahoo.com>
2024-04-26 03:19:30 +0400

    more tests

--- internal/server/handler/validate/validate_test.go
+++ internal/server/handler/validate/validate_test.go
@@ -1,6 +1,10 @@
 package validate
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
 	"testing"
 )
 
@@ -35,6 +39,89 @@ func TestCheckMetricTypeAndName(t *testing.T) {
 			err := CheckMetricTypeAndName(tt.metricType, tt.metricName)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CheckMetricTypeAndName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestGetHashHead(t *testing.T) {
+	// Создаем заголовок с хэшем
+	header := http.Header{}
+	header.Set("HashSHA256", "testHash")
+
+	// Создаем запрос с нашим заголовком
+	req := &http.Request{Header: header}
+
+	tests := []struct {
+		name string
+		args *http.Request
+		want string
+	}{
+		{
+			name: "HashSHA256 header exists",
+			args: req,
+			want: "testHash",
+		},
+		{
+			name: "HashSHA256 header does not exist",
+			args: &http.Request{},
+			want: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetHashHead(tt.args); got != tt.want {
+				t.Errorf("GetHashHead() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCheckHash(t *testing.T) {
+	// Устанавливаем тестовые данные
+	body := []byte("test body")
+	key := "test key"
+	hash := hmac.New(sha256.New, []byte(key))
+	hash.Write(body)
+	expectedHash := hex.EncodeToString(hash.Sum(nil))
+
+	tests := []struct {
+		name    string
+		sha     string
+		body    []byte
+		key     string
+		wantErr bool
+	}{
+		{
+			name:    "Valid hash",
+			sha:     expectedHash,
+			body:    body,
+			key:     key,
+			wantErr: false,
+		},
+		{
+			name:    "Invalid hash",
+			sha:     "invalidHash",
+			body:    body,
+			key:     key,
+			wantErr: true,
+		},
+		{
+			name:    "Empty hash",
+			sha:     "",
+			body:    body,
+			key:     key,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := CheckHash(tt.sha, tt.body, tt.key)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckHash() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 		})
