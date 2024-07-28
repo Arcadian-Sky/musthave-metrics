e73f15c3f9bb7079b71a17061761c4ccaea282e5
ArcadianSky <lllarcadialll@gmail.com>
2024-07-28 19:08:14 +0400

    iter23

---
+++ cmd/server/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServerGracefulShutdown(t *testing.T) {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// Запускаем сервер
+	go main()
+
+	// Отправляем сигнал завершения
+	stop <- syscall.SIGTERM
+
+	// Ожидаем завершения работы
+	time.Sleep(1 * time.Second)
+
+	// Проверяем, что сервер корректно завершился
+
+	assert.True(t, true)
+}
